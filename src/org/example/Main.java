04bea5076f81b8d3b8b27df3b65baab932e6e605
1-21-01-E043-02\POO <mireya.garcia@epn.edu.ec>
2024-05-30 08:44:53 -0500

    version5_objetos

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -9,6 +9,8 @@ public class Main {
         //Integrante1
         procesadores_amd proc_1 = new procesadores_amd();
         procesadores_amd proc_2 = new procesadores_amd(4,1.5,7.5, "Ryzen5","AM4",true);
-
+        //Integrante2
+        procesadores_moviles proc_3 = new procesadores_moviles();
+        procesadores_moviles proc_4 = new procesadores_moviles(3,3.4,5.7,"Raiser08",true);
     }
 }
