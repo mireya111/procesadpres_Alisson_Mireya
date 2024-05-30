e84ca700cabdc8f04cce6e50a7d60817d22d6772
1-21-01-E043-02\POO <mireya.garcia@epn.edu.ec>
2024-05-30 09:31:52 -0500

    Cambio realizado por García Mireya

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -41,9 +41,11 @@ public class Main {
         //Santiago Cumbal Integrante2 Grupo2
 
         //Mireya García Integrante1 Grupo3
+        procesadores_intel pro_3 = new procesadores_intel(5,7.5,5, "Corei7","LGA1158",true);
+        procesadores_intel pro_4 = new procesadores_intel(4,4.5,8, "Corei8","LGA1159",true);
+        procesadores_intel pro_5 = new procesadores_intel(6,6.5,4, "Corei4","LGA1153",false);
 
         //Alisson   Integrante1 Grupo3
-
         procesadores_amd proc_7 = new procesadores_amd(3,10.5,18, "Ryzen9","AM5",false);
         procesadores_amd proc_8 = new procesadores_amd(8,12.5,10, "Epy77","FM1",false);
         procesadores_amd proc_9 = new procesadores_amd(4,13,5, "Threadripper","B650",true);
